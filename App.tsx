67677918ac3781e1b1bbc87a86f2886e4b4d3889
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2024-07-06 14:07:31 +0800

    finish project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+// App.tsx
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTabNavigator from "./src/navigations/BottomTabNavigation";
+
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/Favorite.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import MovieItem from "../components/movies/MovieItem";
+import { useNavigation, useIsFocused } from "@react-navigation/native";
+import type { Movie } from "../types/app";
+
+const { width } = Dimensions.get("window");
+const ITEM_WIDTH = (width - 100) / 3; 
+
+const Favorite = (): JSX.Element => {
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    const fetchFavoriteMovies = async () => {
+      try {
+        const storedFavorites = await AsyncStorage.getItem("@FavoriteList");
+        if (storedFavorites !== null) {
+          const favorites: Movie[] = JSON.parse(storedFavorites);
+          setFavoriteMovies(favorites);
+        }
+      } catch (error) {
+        console.error("Error fetching favorites:", error);
+      }
+    };
+
+    if (isFocused) {
+      fetchFavoriteMovies();
+    }
+  }, [isFocused]);
+
+  const handleMoviePress = (id: number) => {
+    navigation.navigate("MovieDetail", { id });
+  };
+
+  const renderMovieItem = ({ item }: { item: Movie }) => (
+    <TouchableOpacity
+      style={styles.movieItemContainer}
+      onPress={() => handleMoviePress(item.id)}
+    >
+      <MovieItem
+        movie={item}
+        size={{ width: ITEM_WIDTH, height: ITEM_WIDTH * 1.5 }}
+        coverType="poster"
+        onPress={() => handleMoviePress(item.id)} 
+      />
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={favoriteMovies}
+        renderItem={renderMovieItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+        numColumns={3}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+  },
+  listContainer: {
+    flexGrow: 1,
+    paddingHorizontal: 8,
+    paddingBottom: 32,
+    alignItems: "flex-start",
+  },
+  movieItemContainer: {
+    width: ITEM_WIDTH,
+    marginBottom: 8,
+    marginRight: 8,
+  },
+});
+
+export default Favorite;
